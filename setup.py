7459d543ae7e97728b774663eb476cbda14be4a8
Matheus Girardi <girardimatheus27@gmail.com>
2025-08-06 10:50:17 -0300

    fix: Bump version number to 1.0.1 in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="klefki-gen",  # use sempre letras minúsculas aqui para evitar problemas no PyPI
-    version="1.0.0",
+    version="1.0.1",
     description="Secure and customizable password generator.",
     long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
